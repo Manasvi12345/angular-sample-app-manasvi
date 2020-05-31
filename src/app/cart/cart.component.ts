32eca807ceeb6a2c46feb8b4b092198cae9d618f
Manasvi12345 <dedaniamanas123@gmail.com>
2020-05-31 13:37:18 +0530

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -23,7 +23,9 @@ export class CartComponent implements OnInit {
   ngOnInit() {
      this.items = this.cartService.getItems();
   }
+
 onSubmit(customerData) {
+
     // Process checkout data here
     this.items = this.cartService.clearCart();
     this.checkoutForm.reset();
